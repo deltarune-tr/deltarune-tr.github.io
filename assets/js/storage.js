71b29f2732cea99615332d8806913aa61b231484
omerakgoz34 <omerakgoz34@gmail.com>
2019-04-03 00:33:57 +0300

    Fix Releases

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,4 +1,4 @@
-var storage = "<h3>Deltarune-TR \"<font color='yellow'>Don't Forget-Unutma</font>\" Türkçe</h3> </br> <a class='underButton' href='assets/media/audio/dontforget_tr.mp3' download='Deltarune-TR_Dont Forget-Unutma_Türkçe.mp3'>indir</a>"
+﻿var storage = "<h3>Deltarune-TR \"<font color='yellow'>Don't Forget-Unutma</font>\" Türkçe</h3> </br> <a class='underButton' href='assets/media/audio/dontforget_tr.mp3' download='Deltarune-TR_Dont Forget-Unutma_Türkçe.mp3'>indir</a>"
 var wrong = "<h3><font color='red'>Yanlıs Sifre</font></h3>"
 var password = "winofblue"
 
@@ -8,6 +8,6 @@ function openStorage() {
 		document.getElementById("form").innerHTML = "</br>";
 		document.getElementById("storage").innerHTML = storage;
 	} else {
-	      document.getElementById("storage").innerHTML = wrong;
+		  document.getElementById("storage").innerHTML = wrong;
 	  }
 }
